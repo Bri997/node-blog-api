a6d6e5d04e092b8f3f8a587c8d7a848e835d2dab
Brian <brian.musial@gmail.com>
2018-04-26 10:55:59 -0700

    working more on test

--- test/test.js
+++ test/test.js
@@ -26,3 +26,31 @@ it('should GET the blog posts', function () {
     })
 
 })
+
+it('shoud add a blog post on POST', function () {
+    const newBlogPost = {
+        title: "A test title", content: "Some test content", author: "A Test Author"
+    }
+    return chai.request(app)
+    .post('/blog-post')
+    .send(newBlogPost)
+    .then(function(res){
+        expect(res).to.have.status(201);
+        expect(res).to.be.json
+        
+    
+    });
+
+});
+
+it('should fail when POST missing title, content, or author', function () {
+    const newBlogPost = {
+        content: "Missing title", author: "Missing Title"
+    }
+    return chai.request(app)
+    .post('/blog-post')
+    .send(newBlogPost)
+    .then(function (res){
+        expect(res).to.have.status(400);
+    })
+})
